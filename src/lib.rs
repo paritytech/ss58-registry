dcfb3d2107ebc7b4209ab10cc4a9752e7691a66e
gilescope <gilescope@gmail.com>
2021-11-09 08:32:26 +0000

    We should make it clear that the crate is no_std if not std

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#![cfg_attr(not(feature = "std"),no_std)]
 #![warn(missing_docs)]
 
 //! List of well-known SS58 account types as an enum.
