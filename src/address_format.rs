bf811da9bcaf394b5aefeaee4a00fdc3e375c3ef
Xavier Lau <xavier@inv.cafe>
2022-09-23 18:45:58 +0800

    Use `eq_ignore_ascii_case`

--- src/address_format.rs
+++ src/address_format.rs
@@ -105,11 +105,9 @@ impl<'a> TryFrom<&'a str> for Ss58AddressFormatRegistry {
 	type Error = ParseError;
 
 	fn try_from(x: &'a str) -> Result<Ss58AddressFormatRegistry, Self::Error> {
-		let x = x.to_lowercase();
-
 		ALL_SS58_ADDRESS_FORMAT_NAMES
 			.into_iter()
-			.position(|n| n.to_lowercase() == x)
+			.position(|n| n.eq_ignore_ascii_case(&x))
 			.map(|lookup| ALL_SS58_ADDRESS_FORMATS[lookup])
 			.ok_or(ParseError)
 	}
